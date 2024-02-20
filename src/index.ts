2abdd7fbd874a6f7db968d90b6e8b27dbbf2439e
yukun.han <yukun.han@thoughtworks.com>
2024-02-20 09:15:00 +0800

    fix: correct types path

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ import ExtendedMiddlewareHandler from './handlers/extendedMiddlewareHandler.js';
 export const init = () => 'Hello World';
 export { ExtendedJsonHandler, ExtendedMiddlewareHandler };
 export * from './schema/index.js';
+export type * from './types.js';

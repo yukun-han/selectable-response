a4e0eb6a52550b5dbc28c749b97f17cc7649727d
yukun.han <yukun.han@thoughtworks.com>
2024-04-01 22:55:13 +0800

    test: remove collect test coverage

--- jest.config.js
+++ jest.config.js
@@ -8,8 +8,5 @@ export default {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
   testMatch: ['**/tests/**/*.test.ts'],
-  coverageDirectory: 'tests/coverage',
-  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts'],
-  collectCoverage: true,
   maxWorkers: 1,
 };
